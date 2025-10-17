fa76ac0f1054177a4d5fd956c89156d6c43c5ff9
Roxane <davidroxane9@gmail.com>
2025-10-17 08:42:08 -0300

    debut

---
+++ prjetprinter.py
@@ -0,0 +1,8 @@
+option1="option 1 imprimante Canon MegaTank MAXIFY GX7120 au prix de 1139.99$"
+option2="option 2 imprimante Epson EcoTank Pro ET-5800 au prix de 1099.99$"
+print("Bienvenu sur staples que souhaite vous acheter")
+print("option 1 imprimante Canon MegaTank MAXIFY GX7120 au prix de 1139.99$")
+print("option 2 imprimante Epson EcoTank Pro ET-5800 au prix de 1099.99$")
+option_choisie = str(input("Quel option choisisez-vous(1 ou 2): "))
+option="option 1" if option_choisie ==  "1" else "option 2"
+print("vous avez choisi option )
