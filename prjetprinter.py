93f9fb9ecf2a045a16bb15254a667d0af571e43d
Seb200507 <sebastiendoucet2005@gmail.com>
2025-10-18 11:42:27 -0300

    18/10/11h45

--- prjetprinter.py
+++ prjetprinter.py
@@ -1,43 +1,39 @@
 # variable
 option1="imprimante Canon MegaTank MAXIFY GX7120"
-
 option2="imprimante Epson EcoTank Pro ET-5800"
-
 prix_canon=1139.99
-
 prix_epson=1099.99
-
 option3="option 3 offre 2 bouteille d'encre noir par imprimante acheter"
-
 option4="option 4 offre 1 bouteille dencre noir, cyan, magenta et jaune"
-
 option5="je ne vuex pas d'offre"
-
 prix_offre_noir=19.99
-
 prix_offre_couleur=39.98
+
 
 #compagnie binevenu
 print("_" * 50)
-
 print("Bienvenu sur staples que souhaitez-vous acheter")
-
 print("_" * 50)
-
-#option dimprimante
-print("option 1 imprimante Canon MegaTank MAXIFY GX7120 au prix de 1139.99$")
-
 print()
 
-print("option 2 imprimante Epson EcoTank Pro ET-5800 au prix de 1099.99$")
+
+#option dimprimante
+print("option 1 - imprimante Canon MegaTank MAXIFY GX7120 au prix de 1139.99$")
+print()
+print("option 2 - imprimante Epson EcoTank Pro ET-5800 au prix de 1099.99$")
+print()
+
 
 #input choisire son option
 option_choisie = str(input("- Quel option choisisez-vous(1 ou 2): "))
+print()
+
 
 #affiche option choisi
 option= option1 if option_choisie == "1" else option2
 print(f"- vous avez choisi option: {option}")
 print("_" * 50)
+
 
 #quantiter dimprimante acheter
 quantiter=int(input("- Combien imprimante souhaitez-vous acheter: "))
@@ -47,20 +43,15 @@ if option_choisie == "1":
 else:
     print(f"- voici combien vont couter vos imprimante {quantiter*prix_epson:.2f}")
 print("_" * 50)
-    
+
+
 #offre dencre
 print("- voici nos offre d'encre a l'achat d'une imprimante")
-
 print()
-
 print("option 3 offre 2 bouteille d'encre noir")
-
 print()
-
 print("option 4 offre 1 bouteille dencre noir, cyan, magenta et jaune")
-
 print()
-
 print("option 5 vous ne voulez pas profiter de l'offre d'encre")
 
 offre_choisie = str(input("- Quel option choisisez-vous(3, 4 ou 5 ): "))
