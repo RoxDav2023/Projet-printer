c177d69cc1099ec51330d7a0dd6a861eadc98279
Seb200507 <sebastiendoucet2005@gmail.com>
2025-10-18 21:04:49 -0300

    18/10/9h

--- prjetprinter.py
+++ prjetprinter.py
@@ -14,6 +14,7 @@ prix_offre_couleur=39.98
 
 #compagnie binevenu
 print("_" * 50)
+print()
 print("Bienvenu sur staples que souhaitez-vous acheter")
 print("_" * 50)
 print()
@@ -35,16 +36,20 @@ print()
 option= option1 if option_choisie == "1" else option2
 print(f"- vous avez choisi option: {option}")
 print("_" * 50)
+print()
 
 
 #quantiter dimprimante acheter
 quantiter=int(input("- Combien imprimante souhaitez-vous acheter: "))
+print()
 print(f"- vous avez indiquer vouloir {quantiter} {option} ")
+print()
 if option_choisie == "1":
     print(f"- voici combien vont couter vos imprimante {quantiter*prix_canon:.2f}")
 else:
     print(f"- voici combien vont couter vos imprimante {quantiter*prix_epson:.2f}")
 print("_" * 50)
+print()
 
 
 #offre dencre
@@ -55,9 +60,10 @@ print()
 print("option 4 offre 1 bouteille dencre noir, cyan, magenta et jaune")
 print()
 print("option 5 vous ne voulez pas profiter de l'offre d'encre")
+print()
 
 offre_choisie = str(input("- Quel option choisisez-vous(3, 4 ou 5 ): "))
-
+print()
 if offre_choisie == "3":
     offre = option3
     print("Vous avez choisi l'offre contenant une bouteille d'encre noir")
@@ -69,6 +75,7 @@ else:
     print("vous avez refusez l'offre")
 
 print("_" * 50)
+print()
 
 #Calcul du prix selon l'offre
 if option_choisie == "1":
@@ -87,21 +94,27 @@ prix_offretot = ((prix_imprimente + prix_offre) * quantiter)
 
 #Calcul 
 if quantiter >= 50:
-    prixtotal = (prix_offretot - (prix_offretot * 0.5))
+    prixtotal = (prix_offretot - (prix_offretot * 0.05))
 else:
     prixtotal = prix_offretot
 
 
-    #Sortie des donnees
-    print(f"Sous total des imprimentes: {prix_imprimente * quantiter:.2f} ")
-    if offre_choisie == "3":
-        print(f"Le sous total des cartouches est: {prix_offre_noir * quantiter:.2f}")
-    elif offre_choisie == "4":
-        print(f"Le sous total des cartouches est de: {prix_offre_couleur * quantiter: .2f}")
-    else:
-        print("Le sous total des cartouche est de: 0.00")
-    if quantiter >= 50:
-        print(f"Rabais: {prix_offretot * 0.5:.2f}")
-    else:
-        print("Rabais: 0.00")
-    print(f"Sous total incluant le rabais et cartouches: {prixtotal}")
+#Sortie des donnees
+print(f"Sous total des imprimentes: {prix_imprimente * quantiter:.2f} ")
+print()
+if offre_choisie == "3":
+    print(f"Le sous total des cartouches est: {prix_offre_noir * quantiter:.2f}")
+elif offre_choisie == "4":
+    print(f"Le sous total des cartouches est de: {prix_offre_couleur * quantiter: .2f}")
+else:
+    print("Le sous total des cartouche est de: 0.00")
+print()
+
+if quantiter >= 50:
+    print(f"Rabais: {prix_offretot * 0.05:.2f}")
+else:
+    print("Rabais: 0.00")
+print()
+print(f"Sous total incluant le rabais et cartouches: {prixtotal}")
+print()
+print()
